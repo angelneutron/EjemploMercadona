046b90dda859e81266b6d743dc78ac2da28322ed
usuario <usuario@192.168.1.40>
2016-11-10 09:42:16 +0100

    Modificación para probar

--- src/mercadona/Mercadona.java
+++ src/mercadona/Mercadona.java
@@ -118,6 +118,12 @@ public class Mercadona {
     /**
      * @param args the command line arguments
      */
+    
+    
+    
+    
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
 
